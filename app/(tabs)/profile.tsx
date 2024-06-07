850c402fd142af7a6f8152f89f3f50f7a31e6888
tanaliga100 <43674051+tanaliga100@users.noreply.github.com>
2024-06-07 18:03:33 +0800

    add-instanec

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,11 @@
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import React from "react";
+
+export default function ProfileScreen() {
+  return (
+    <ThemedView>
+      <ThemedText type="title">Profile Screen</ThemedText>
+    </ThemedView>
+  );
+}
